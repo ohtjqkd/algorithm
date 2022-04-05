a90603c8f8b068d8bbad0986afee95883f5fb0ff
ohtjqkd <ohtjqkd@gmail.com>
2022-04-05 11:42:28 +0900

    21944 finish, but failed, to do 21943

--- 42study/week7/21944.py
+++ 42study/week7/21944.py
@@ -1,219 +1,130 @@
-from bisect import bisect, bisect_right, insort
-from collections import deque, defaultdict
-import difflib
-from multiprocessing.connection import wait
-import sys
-input = sys.stdin.readline
-write = sys.stdout.write
-# test case가 부족해..
-# 틀림
-
-from collections import defaultdict
-import sys
-import heapq
+import sys, heapq
 
 si = sys.stdin.readline
 def MIIS(): return map(int, si().split())
 
+class CurrentInfo:
+    def __init__(self):
+        self.num_list = [[0 for _ in range(2)] for _ in range(100001)]
+        self.record = [dict() for _ in range(100001)]
+    
+    def solve(self, num):
+        self.num_list[num][0], self.num_list[num][1] = 0, 0
+    
+    def add(self, num, diff, algo_num):
+        if self.record[num].get((diff, algo_num), False):
+            return
+        if self.num_list[num][0] == 0 and self.num_list[num][1] == 0:
+            self.num_list[num][0], self.num_list[num][1] = diff, algo_num
+            self.record[num][(diff, algo_num)] = True    
+    
+    def is_in_heap(self, heap_info, searching_type):
+        diff, num, algo_num = map(lambda x: x * -searching_type, heap_info)
+        if self.num_list[num][0] == diff and self.num_list[num][1] == algo_num:
+            return True
+        return False
+
+class Recommend1:
+    def __init__(self, limit = 100, info = CurrentInfo):
+        self.info = info
+        self.limit = limit
+        self.max_heap = [[] for _ in range(self.limit + 1)]
+        self.min_heap = [[] for _ in range(self.limit + 1)]
+    
+    def push(self, num, diff, algo_num):
+        heapq.heappush(self.max_heap[algo_num], (-diff, -num, -algo_num))
+        heapq.heappush(self.min_heap[algo_num], (diff, num, algo_num))
+
+    def get_num_by_algo(self, algo_num, searching_type):
+        if searching_type == 1:
+            target_heap = self.max_heap[algo_num]
+        elif searching_type == -1:
+            target_heap = self.min_heap[algo_num]
+        while target_heap:
+            if self.info.is_in_heap(target_heap[0], searching_type):
+                return -searching_type * target_heap[0][1] 
+            heapq.heappop(target_heap)
+
+class Recommend2:
+    def __init__(self, limit = 100001, info = CurrentInfo):
+        self.info = info
+        self.limit = limit
+        self.max_heap = []
+        self.min_heap = []
+    
+    def push(self, num, diff, algo_num):
+        heapq.heappush(self.max_heap, (-diff, -num, -algo_num))
+        heapq.heappush(self.min_heap, (diff, num, algo_num))
+
+    def get_num(self, searching_type):
+        if searching_type == 1:
+            target_heap = self.max_heap
+        elif searching_type == -1:
+            target_heap = self.min_heap
+        while target_heap:
+            if self.info.is_in_heap(target_heap[0], searching_type):
+                return -searching_type * target_heap[0][1]
+            heapq.heappop(target_heap)
+
+class Recommend3:
+    def __init__(self, limit = 100, info = CurrentInfo):
+        self.info = info
+        self.limit = limit
+        self.max_heap = [[] for _ in range(self.limit + 1)]
+        self.min_heap = [[] for _ in range(self.limit + 1)]
+    
+    def push(self, num, diff, algo_num):
+        heapq.heappush(self.max_heap[diff], (-diff, -num, -algo_num))
+        heapq.heappush(self.min_heap[diff], (diff, num, algo_num))
+
+    def get_num_from_diff(self, diff, searching_type):
+        if searching_type == 1:
+            target_heap = self.min_heap
+        elif searching_type == -1:
+            target_heap = self.max_heap
+        while 0 <= diff < self.limit + 1:
+            while target_heap[diff]:
+                if self.info.is_in_heap(target_heap[diff][0], -searching_type):
+                    return searching_type * target_heap[diff][0][1]
+                heapq.heappop(target_heap[diff])
+            diff += searching_type
+        return -1
 
 n = int(si())
 
-min_heap = []
-max_heap = []
-diff_heap = [[[], []] for _ in range(101)]
-algo_heap = [[[], []] for _ in range(101)]
-ret = []
-num_list = [[0,0] for _ in range(100001)]
-visited = defaultdict()
+info = CurrentInfo()
+recom_1 = Recommend1(info = info)
+recom_2 = Recommend2(info = info)
+recom_3 = Recommend3(info = info)
 
 for _ in range(n):
     num, difficulty, algo_num = MIIS()
-    heapq.heappush(min_heap, (difficulty, num, algo_num))
-    heapq.heappush(max_heap, (-difficulty, -num, -algo_num))
-    heapq.heappush(algo_heap[algo_num][0], (difficulty, num, algo_num))
-    heapq.heappush(algo_heap[algo_num][1], (-difficulty, -num, -algo_num))
-    heapq.heappush(diff_heap[difficulty][0], (difficulty, num, algo_num))
-    heapq.heappush(diff_heap[difficulty][1], (-difficulty, -num, -algo_num))
+    recom_1.push(num, difficulty, algo_num)
+    recom_2.push(num, difficulty, algo_num)
+    recom_3.push(num, difficulty, algo_num)
+    info.add(num, difficulty, algo_num)
 
-    visited[num] = [difficulty, algo_num]
 m = int(si())
 for _ in range(m):
     line = si().split()
     if line[0] == 'add':
         num, difficulty, algo_num = map(int, line[1:])
-        heapq.heappush(min_heap, (difficulty, num, algo_num))
-        heapq.heappush(max_heap, (-difficulty, -num, -algo_num))
-        heapq.heappush(algo_heap[algo_num][0], (difficulty, num, algo_num))
-        heapq.heappush(algo_heap[algo_num][1], (-difficulty, -num, -algo_num))
-        heapq.heappush(diff_heap[difficulty][0], (difficulty, num, algo_num))
-        heapq.heappush(diff_heap[difficulty][1], (-difficulty, -num, -algo_num))        
-        visited[num] = [difficulty, algo_num]
-
+        recom_1.push(num, difficulty, algo_num)
+        recom_2.push(num, difficulty, algo_num)
+        recom_3.push(num, difficulty, algo_num)
+        info.add(num, difficulty, algo_num)
+        
     elif line[0] == 'solved':
-        visited[int(line[1])] = [0, 0]
-    elif line[0] == 'recommend2':
-        if line[1] == '1':
-            while max_heap:
-                diff, num, algo_num = max_heap[0]
-                if visited[-num][0] == -diff and visited[-num][1] == -algo_num:
-                    ret.append(-num)
-                    break
-                heapq.heappop(max_heap)
-        elif line[1] == '-1':
-            while min_heap:
-                diff, num, algo_num = min_heap[0]
-                if visited[num][0] == diff and visited[num][1] == algo_num:
-                    ret.append(num)
-                    break
-                heapq.heappop(min_heap)
+        info.solve(int(line[1]))
 
     elif line[0] == 'recommend':
-        algo_num, want = map(int, line[1:])
-        if want == 1:
-            while algo_heap[algo_num][1]:
-                diff, num, t_algo_num = algo_heap[algo_num][1][0]
-                if visited[-num][0] == -diff and visited[-num][1] == -t_algo_num:
-                    ret.append(-num)
-                    break
-                heapq.heappop(algo_heap[algo_num][1])
-        elif want == -1:
-            while algo_heap[algo_num][0]:
-                diff, num, t_algo_num = algo_heap[algo_num][0][0]
-                if visited[num][0] == diff and visited[num][1] == t_algo_num:
-                    ret.append(num)
-                    break
-                heapq.heappop(algo_heap[algo_num][0])
+        algo_num, searching_type = map(int, line[1:])
+        print(recom_1.get_num_by_algo(algo_num, searching_type))
+
+    elif line[0] == 'recommend2':
+        searching_type = int(line[1])
+        print(recom_2.get_num(searching_type))
+
     elif line[0] == 'recommend3':
-        want, l = map(int, line[1:])
-        if want == 1:
-            for i in range(l, 101):
-                check = False
-                while diff_heap[i][1]:
-                    diff, num, algo_num = diff_heap[i][1][0]
-                    if visited[-num][0] == -diff and visited[-num][1] == -algo_num:
-                        ret.append(-num)
-                        check =True
-                        break
-                    heapq.heappop(diff_heap[i][1])
-                if check:
-                    break
-            else:
-                ret.append(-1)
-        if want == -1:
-            for i in range(l, 0, -1):
-                check = False
-                while diff_heap[i][0]:
-                    diff, num, algo_num = diff_heap[i][0][0]
-                    if visited[num][0] == diff and visited[num][1] == algo_num:
-                        ret.append(num)
-                        check =True
-                        break
-                    heapq.heappop(diff_heap[i][0])
-                if check:
-                    break
-            else:
-                ret.append(-1)
-
-for ans in ret:
-    print(ans)
-
-
-
-
-# N = int(input())
-
-# where_number = [0] * 100001
-# command = []
-# init_pair = []
-# total_pair = []
-
-# diff_list = [[] for _ in range(101)]
-# algo_list = [[] for _ in range(101)]
-# num_list = [[0, 0] for _ in range(100001)]
-# most_min = float('inf')
-# most_max = float('-inf')
-# for i in range(N):
-#     num, dif, algo_num = map(int, input().split(" "))
-#     num_list[num] = [dif, algo_num]
-#     insort(algo_list[algo_num], (dif, num))
-#     insort(diff_list[dif], num)
-#     most_min = min(most_min, dif)
-#     most_max = max(most_max, dif)
-
-# # print(algo_list)
-# M = int(input())
-
-# for i in range(M):
-#     # print()
-#     # print()
-#     line = input().rstrip().split(" ")
-#     com = line[0]
-#     # print(i, "th command: ", com, line[1:])
-
-#     if com == "add":
-#         num, dif, algo_num = map(int, line[1:])
-#         insort(algo_list[algo_num], (dif, num))
-#         insort(diff_list[dif], num)
-#         num_list[num] = [dif, algo_num]
-#         most_min = min(most_min, dif)
-#         most_max = max(most_max, dif)
-#         # print("most_min ", most_min, " most_max ", most_max)
-
-#     elif com == "solved":
-#         num = int(line[1])
-#         dif, algo_num = num_list[num][0], num_list[num][1]
-#         # print("now dif ", dif, " now algo num ", algo_num)
-#         idx = bisect_right(algo_list[algo_num], (dif, num))
-#         # print("algo_list", algo_list, "idx", idx)
-
-#         del(algo_list[algo_num][idx-1])
-#         idx = bisect_right(diff_list[dif], num)
-#         del(diff_list[dif][idx-1])
-#         if dif == most_min and len(diff_list[dif]) == 0:
-#             for i in range(dif, 101):
-#                 if len(diff_list[i]) != 0:
-#                     most_min = i
-#                     break
-#             else:
-#                 most_min = float('inf')
-#         if dif == most_max and len(diff_list[dif]) == 0:
-#             for i in range(dif, 0, -1):
-#                 if len(diff_list[i]) != 0:
-#                     most_max = i
-#                     break
-#             else:
-#                 most_max = float('-inf')
-#         # print("most_min ", most_min, " most_max ", most_max)
-#         num_list[num][0], num_list[num][1] = 0, 0
-#     elif com == "recommend":
-#         algo_num, want = map(int, line[1:])
-#         if want == -1:
-#             print(algo_list[algo_num][0][1])
-#         else:
-#             print(algo_list[algo_num][-1][1])
-#     elif com == "recommend2":
-#         want = int(line[1])
-#         if want == -1:
-#             print(diff_list[most_min][0])
-#         else:
-#             print(diff_list[most_max][-1])
-#     elif com == "recommend3":
-#         want, l = map(int, line[1:])
-#         if want == -1:
-#             for i in range(l, most_min - 1 , -1):
-#                 if len(diff_list[i]) != 0:
-#                     print(diff_list[i][-1])
-#                     break
-#             else:
-#                 print(-1)
-#         else:
-#             for i in range(l, most_max + 1):
-#                 if len(diff_list[i]) != 0:
-#                     print(diff_list[i][0])
-#                     break
-#             else:
-#                 print(-1)
-    # print("algo_list")
-    # print(algo_list)
-    # print("diff_list")
-    # print(diff_list)
+        searching_type, l = map(int, line[1:])
+        print(recom_3.get_num_from_diff(l, searching_type))
